950987c2bacb688b4415033c01ea16573ff92d0b
My name <example@example.com>
2021-03-25 16:43:41 -0500

    Also to do:  teams, blogs, steps

--- listfiles.py
+++ listfiles.py
@@ -1,2 +1,3 @@
 import os
-[print("{%- include 'components/kitwind_all/contents/" + x.split('.')[0] + "' section:section_comprehensive -%}\r\n") for x in os.listdir('./src/_includes/components/kitwind_all/contents')]
+foldername = 'teams'
+[print("{%- include 'components/kitwind_all/" + foldername + "/" + x.split('.')[0] + "' section:section_comprehensive -%}\r\n") for x in os.listdir('./src/_includes/components/kitwind_all/' + foldername)]
